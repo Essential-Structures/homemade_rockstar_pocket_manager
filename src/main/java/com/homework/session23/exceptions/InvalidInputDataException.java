44b6c8bee5bf681e88c59fe8e42412d0820c82a2
SabinBadea <sabins.badea@gmail.com.com>
2024-10-22 17:32:34 +0300

    working on POST exception handling

---
+++ src/main/java/com/homework/session23/exceptions/InvalidInputDataException.java
@@ -0,0 +1,8 @@
+package com.homework.session23.exceptions;
+
+public class InvalidInputDataException extends RuntimeException{
+
+     public InvalidInputDataException (String message) {
+         super (message);
+     }
+}
